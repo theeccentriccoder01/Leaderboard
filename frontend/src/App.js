7be9fa502f935c8334ca0fd6a55f5ba56979deba
theeccentriccoder01 <ceo@edulinkup.dev>
2025-07-30 12:29:51 +0530

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     const [pointsAwarded, setPointsAwarded] = useState(null);
     const [newUserName, setNewUserName] = useState('');
 
-    const API_BASE_URL = 'http://localhost:5000/api/users';
+    const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/api/users`;
 
     const fetchLeaderboard = async () => {
         try {
